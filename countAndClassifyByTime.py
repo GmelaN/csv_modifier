731b67f4731e2523c5fae7c5b4d0de90b99fa4cf
gmelan <gmelanals@gmail.com>
2021-02-27 21:04:51 +0900

    some stablity improvement

--- countAndClassifyByTime.py
+++ countAndClassifyByTime.py
@@ -12,8 +12,8 @@ TIME = -1
 
 csvFile = "./newsList_raw"
 
-f = open(csvFile + ".csv", 'r', encoding="utf8", newline="")
-c = list(csv.reader(f))
+with open(csvFile + ".csv", 'r', encoding="utf8", newline="") as f:
+    c = list(csv.reader(f))
 
 wordList = {
     "코로나": 0, "코로나 장전": 0, "코로나 장중": 0, "코로나 장외": 0,
@@ -28,27 +28,35 @@ wordList = {
     "신규":0, "신규 장전":0, "신규 장중":0, "신규 장외":0
     }
 
+wordList_keywords = [ list(wordList.keys())[i] for i in range(0, len(wordList.keys()), 4) ]
 
 
-listToWrite = [([i for i in wordList.keys()] + ["날짜", "시각"])]
+
+listToWrite = [([i for i in wordList.keys()] + ["날짜", "시각"])] # 기록할 최종 리스트 - 헤더 행을 미리 append한 상태임
 
 for news in c[1:]:
-    for word in [list(wordList.keys())[i] for i in range(0, len(wordList.keys()), 4)]:
-        sum_ = news[TITLE].count(word) + news[SUBTITLE].count(word)
-        # 총합
+    for word in wordList_keywords:
+        newsString = news[TITLE] + news[SUBTITLE] # 한 기사의 제목, 부제 행 정보를 담은 문자열
+        sum_ = newsString.count(word) # newsString으로부터 키워드 횟수를 구함
+
+        # sum_ = news[TITLE].count(word) + news[SUBTITLE].count(word)
+
+        # 한 기사에서 word가 나온 횟수를, word 횟수의 총합 변수에 더함
         wordList[word] += sum_
 
-        # 시간대별 합
+        # 시간대별 합 => 해당 기사의 개재 시각을 봄
         time = int(news[TIME])
-        if time < 90000:
+        if 0 <= time < 90000:
             timeStr = " 장전"
-        elif time < 153000:
+        elif 90000 <= time < 153000:
             timeStr = " 장중"
-        else:
+        elif 153000 <= time <= 235959:
             timeStr = " 장외"
 
+        #wordList의 "### 장*" 항목에 한 기사에서 word가 나온 횟수를 더함
         wordList[word + timeStr] += sum_
 
+    # write할 최종 리스트에 현재 기사의 키워드 빈도 결과 리스트를 append
     listToWrite.append(
         list(wordList.values()) + [news[DATE], news[TIME]]
         )
@@ -60,4 +68,4 @@ for news in c[1:]:
 with open(csvFile + '_count.csv', 'w', encoding="utf8", newline="") as w:
     csv.writer(w).writerows(listToWrite)
 
-f.close()
+print("작업 완료, 다음에서 찾을 수 있음:", csvFile + '_count.csv')
